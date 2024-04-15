af51cdfae1f0fa8df0dcf1d2f3ba0229122b19ea
Player0neTwo <167133389+Player0neTwo@users.noreply.github.com>
2024-04-15 16:29:12 -0400

    commit

---
+++ data.js
@@ -0,0 +1,94 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20240322_150842_00_005",
+      "name": "IMG_20240322_150842_00_005",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.2719037814403418,
+        "pitch": 0.1497345860354109,
+        "fov": 1.3248986771387785
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9964958679552742,
+          "pitch": 0.2341748021338983,
+          "rotation": 0,
+          "target": "1-img_20240322_151004_00_007"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20240322_151004_00_007",
+      "name": "IMG_20240322_151004_00_007",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.4462703522847544,
+        "pitch": 0.12544092304790766,
+        "fov": 1.3248986771387785
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.33402418999291683,
+          "pitch": 0.16197110121270697,
+          "rotation": 0,
+          "target": "0-img_20240322_150842_00_005"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5810610962259801,
+          "pitch": 0.7628171508592541,
+          "title": "CAT",
+          "text": "cat"
+        }
+      ]
+    }
+  ],
+  "name": "TEST",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
